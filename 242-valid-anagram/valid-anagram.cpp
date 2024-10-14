d3d84dec4115a4bdf57c984eeed2b4936118865e
Niteesh Kulhari <nkulhari96@gmail.com>
2024-10-14 06:26:04 -0400

    Time: 4 ms (90.12%) | Memory: 10.5 MB (16.68%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -5,15 +5,36 @@ public:
             return false;
         }
 
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        // Sorting Method than comparing
 
-        for (int i=0; i<s.length(); i++){
-            if(s[i] != t[i]){
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+
+        // for (int i=0; i<s.length(); i++){
+        //     if(s[i] != t[i]){
+        //         return false;
+        //     }
+        // }
+
+
+        // return true;
+
+
+        // Unordered map way
+
+        unordered_map<char, int> sMap;
+        unordered_map<char, int> tMap;
+
+        for(int i=0; i<s.length(); i++){
+            sMap[s[i]]++;
+            tMap[t[i]]++;
+        }
+
+        for(int i=0; i<sMap.size(); i++){
+            if(sMap[i] != tMap[i]){
                 return false;
             }
         }
-
 
         return true;
     }
