87e1166ff677e9a422a74732e1aa1c3a6875a9df
Niteesh Kulhari <nkulhari96@gmail.com>
2024-10-23 12:13:37 -0400

    Time: 14 ms (99.14%) | Memory: 49.8 MB (86.04%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -11,7 +11,7 @@ public:
         int currentStreak = 1;
 
         for(int i=1; i<n; i++){
-
+            
             if(nums[i] != nums[i-1]){
                 if( nums[i] == nums[i-1] + 1){
                 currentStreak++;
