a097619b990ae6f93014be554508f3bce722864f
Niteesh Kulhari <nkulhari96@gmail.com>
2024-10-27 10:37:20 -0400

    Time: 1702 ms (5.18%) | Memory: 82.4 MB (74.79%) - LeetSync

---
+++ 883-car-fleet/car-fleet.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int,float>> positions;
+        int n = position.size();
+
+        for(int i=0; i<n; i++){
+            double time=(double)(target-position[i])/speed[i];
+            positions.push_back({position[i], time});
+        }
+        sort(positions.begin(), positions.end());
+        for( auto pos: positions){
+            cout << "Position: " << pos.first << " Time: " << pos.second << endl;
+        }
+
+        float maxTime = 0.0;
+        int count = 0;
+        for(int i=n-1; i>=0; i--){
+            float time = positions[i].second;
+
+            if(time > maxTime){
+                count++;
+                maxTime = time;
+            }
+        }
+
+        return count;
+    }
+};
