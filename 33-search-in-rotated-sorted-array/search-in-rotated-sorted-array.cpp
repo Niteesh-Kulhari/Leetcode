bf1d5ae4f9923d1d79b79fb768ca0d47d9d18cdc
Niteesh Kulhari <nkulhari96@gmail.com>
2024-10-28 18:50:11 -0400

    Time: 0 ms (100.00%) | Memory: 13.8 MB (95.29%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,52 +1,54 @@
 class Solution {
-public:
-    int binarySearch(vector<int> &nums, int left, int right, int target){
-        while(left <=  right){
-            int mid = left + (right-left)/2;
+    private:
+    int binarySearch(vector<int> &nums, int start, int end, int target){
+        while(start <= end){
+            int mid = start + (end - start)/2;
 
             if(nums[mid] == target){
                 return mid;
-            }
-
-            if(nums[mid] > target){
-                right = mid -1;
+            }else if(nums[mid] < target){
+                start = mid+1;
             }else{
-                left = mid + 1;
+                end = mid-1;
             }
         }
 
         return -1;
     }
-
+public:
     int search(vector<int>& nums, int target) {
-        if(nums.size() == 1){
-            if(nums[0] == target){
+        int n = nums.size();
+
+        if(n == 1){
+            if(target == nums[0]){
                 return 0;
             }else{
                 return -1;
             }
         }
-        int n = nums.size();
+
         int left = 0;
-        int right = nums.size() - 1;
+        int right = n-1;
 
         while(left <= right){
-            int mid = left + (right - left)/2;
-
+            int mid = left + (right-left)/2;
+            
             if(nums[mid] > nums[n-1]){
-                left = mid + 1;
+                left = mid+1;
+            }else{
+                right = mid - 1;
             }
-            else{
-                right = mid -1;
-            }
-        }
-        
-        int ans = binarySearch(nums, 0, left-1, target);
 
-        if(ans!=-1){
+        }
+
+        int ans = binarySearch(nums, 0, left-1, target);
+        if(ans != -1){
             return ans;
         }
-        
+
+
         return binarySearch(nums, left, n-1, target);
+
+
     }
 };
