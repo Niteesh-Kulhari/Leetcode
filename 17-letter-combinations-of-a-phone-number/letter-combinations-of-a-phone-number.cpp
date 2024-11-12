f5c14b22df48ca45387300f68d9ee2d35e976a6e
Niteesh Kulhari <nkulhari96@gmail.com>
2024-11-12 02:31:54 -0500

    Time: 0 ms (100.00%) | Memory: 8.7 MB (26.56%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -2,10 +2,10 @@ class Solution {
 private:
     string phoneDigits;
     vector<string> ans;
-    //vector<string> comb = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-    unordered_map<char, string> comb = {
-        {'2', "abc"}, {'3', "def"},  {'4', "ghi"}, {'5', "jkl"},
-        {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}};
+    vector<string> comb = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    // unordered_map<char, string> comb = {
+    //     {'2', "abc"}, {'3', "def"},  {'4', "ghi"}, {'5', "jkl"},
+    //     {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}};
 public:
     vector<string> letterCombinations(string digits) {
         if(digits.length() == 0){
@@ -23,11 +23,14 @@ public:
             ans.push_back(path);
         }
         
-        string chars = comb[phoneDigits[index]];
-        for(auto ch: chars){
-            path.push_back(ch);
-            backTrack(index+1, path);
-            path.pop_back();
+        if (index < phoneDigits.length()) {
+            // Get characters that the current digit maps to, and iterate over them
+            string chars = comb[phoneDigits[index] - '0'];
+            for (char ch : chars) {
+                path.push_back(ch);         // Choose
+                backTrack(index + 1, path); // Explore
+                path.pop_back();            // Un-choose (backtrack)
+            }
         }
     }
 };
