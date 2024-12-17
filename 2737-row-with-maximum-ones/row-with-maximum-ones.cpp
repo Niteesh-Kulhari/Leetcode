dff58982f70762d0f122281aa5a691f5e1d062e2
Niteesh Kulhari <nkulhari96@gmail.com>
2024-12-17 17:33:49 -0500

    Time: 0 ms (100.00%) | Memory: 64.5 MB (97.21%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
-        priority_queue<pair<int, int>> maxHeap;
-
         int n = mat.size();
         int m = mat[0].size();
 
+        int ansCount = 0;
+        int ansRow = 0;
         for(int i=0; i<n; i++){
             int count = 0;
             for(int j=0; j<m; j++){
-                if(mat[i][j] == 1){
-                    count++;
-                }
+                count += mat[i][j];
+            }
 
-                maxHeap.push({count, -i});
+            if(count > ansCount){
+                ansCount = count;
+                ansRow = i;
             }
         }
-        auto top = maxHeap.top();
-        return {-top.second, top.first};
+
+        return {ansRow, ansCount};
     }
 };
