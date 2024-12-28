4a2c12c4db097c6c642f62f22e1bada0cfae9d4c
Niteesh Kulhari <nkulhari96@gmail.com>
2024-12-28 10:42:13 -0500

    Time: 791 ms (37.55%) | Memory: 11.5 MB (52.41%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,51 +1,46 @@
 class Solution {
-public:
-    bool exist(vector<vector<char>>& board, string word) {
-        int n = board.size();
-        int m = board[0].size();
-
-        for(int i = 0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(backtrack(board, i, j, word, 0)){
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
-
-    bool backtrack(vector<vector<char>>& board,int row, int col, string word, int index){
-        if(index == word.length()){
+private:
+    bool backTrack(vector<vector<char>>& board, string word, int row, int col, int index){
+        if( index == word.length() ){
             return true;
         }
-        
+
         int n = board.size();
         int m = board[0].size();
-
+        // Check if row and col are inside the bounds
         if(row < 0 || row >= n || col < 0 || col >= m || board[row][col] != word[index]){
             return false;
         }
 
         char temp = board[row][col];
         board[row][col] = '#';
-        
-
-        int delRow[] = {-1, 0, 1, 0};
-        int delCol[] = {0, 1, 0, -1};
-
+        // Movement Left, Right, Up, Down
+        int delRow[] = {0, 0, -1, 1};
+        int delCol[] = {-1, 1, 0, 0};
 
         for(int i=0; i<4; i++){
-            int newRow = row + delRow[i];
-            int newCol = col + delCol[i];
+            int newRow = delRow[i] + row;
+            int newCol = delCol[i] + col;
 
-            if(backtrack(board, newRow, newCol, word, index+1)){
+            if(backTrack(board, word, newRow, newCol, index+1)){
                 return true;
             }
         }
-
         board[row][col] = temp;
         return false;
 
+    }
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        int n = board.size();
+        int m = board[0].size();
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(backTrack(board, word, i, j, 0)) return true;
+            }
+        }
+
+        return false;
     }
 };
