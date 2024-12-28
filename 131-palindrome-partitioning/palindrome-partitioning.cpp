606fc4c0608249d47351e2c4d8742d65f60cdce6
Niteesh Kulhari <nkulhari96@gmail.com>
2024-12-28 17:17:26 -0500

    Time: 1139 ms (5.01%) | Memory: 82.3 MB (26.07%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,46 @@
+class Solution {
+private:
+    bool isPalindrome(string& s, int start, int end){
+        while(start < end){
+            if(s[start] != s[end]){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    void backTrack(vector<vector<string>>& ans, vector<string>& temp, int index, string s){
+        if(index >= s.length()){
+            ans.push_back(temp);
+            return;
+        }
+
+        for(int i=index; i<s.length(); i++){
+            if(isPalindrome(s, index, i)){
+                temp.push_back(s.substr(index, i-index+1));
+                backTrack(ans, temp, i+1, s);
+                temp.pop_back();
+            }
+        }
+
+
+    }
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> temp;
+
+        backTrack(ans, temp, 0, s);
+
+        for(auto vec: ans){
+            for(auto s: vec){
+                cout << s << " ";
+            }
+            cout << endl;
+        }
+
+        return ans;
+    }
+};
