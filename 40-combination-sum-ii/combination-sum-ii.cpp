9a2534c07ed777f696695d04b8c7eab1f615a2ca
Niteesh Kulhari <nkulhari96@gmail.com>
2024-12-27 20:53:43 -0500

    Time: 3 ms (43.94%) | Memory: 14.1 MB (44.80%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    void backTrack(vector<int>& candidates, vector<vector<int>>& ans, vector<int>& comb, int index, int remain){
+        if(remain == 0){
+            ans.push_back(comb);
+        }else if( remain < 0){
+            return;
+        }
+
+        for(int i=index; i<candidates.size(); i++){
+
+            if(i>index && candidates[i] == candidates[i-1]){
+                continue;
+            }
+            comb.push_back(candidates[i]);
+            backTrack(candidates, ans, comb, i+1, remain - candidates[i]);
+            comb.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> comb;
+        sort(candidates.begin(), candidates.end());
+        backTrack(candidates, ans, comb, 0, target);
+
+        return ans;
+    }
+};
